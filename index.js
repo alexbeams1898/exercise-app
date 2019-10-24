246379eb3fad737f6ded7ddc30e3229e19ef47dd
alexbeams1898 <beamsa1@hawkmail.newpaltz.edu>
2019-10-24 08:23:42 -0400

    final commit

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ mongoose.connect(
 );
 mongoose.Promise = global.Promise;
 
-app.get("/", function(req, res) {
-  res.sendfile("./public/index.html");
-});
+// app.get("/", function(req, res) {
+//   res.sendfile("./public/index.html");
+// });
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -43,20 +43,20 @@ app.use((req, res, next) => {
 app.use("/users", userRoutes);
 app.use("/exercises", exerciseRoutes);
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not found");
+//   error.status = 404;
+//   next(error);
+// });
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+// });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public/"));
